081af40bf821b696832c89283895674cc3b47e98
Will Taylor <taylorw@udel.edu>
2019-01-09 23:45:10 -0500

    Based title add navbar off pages json

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Arrow from './down-arrow.svg';
 import InfoSection from './InfoSection/InfoSection';
 import Rating from './Rating/Rating';
 import Languages from './topLanguages.json';
+import Pages from './pages.json';
 import NavBar from './NavBar/NavBar';
 import $ from 'jquery';
 
@@ -24,22 +25,26 @@ class App extends Component {
           <h1 className="main-header">Hi, I'm <span className="accent">Will</span>.</h1>
           <h2 className="secondary-header">Let me tell you about myself.</h2>
           <button onClick={this.arrowClickHandler} className="link-button">
-            <img section="intro" src={Arrow} alt="arrow-down" className="indicator"></img>
+            <img section={Pages[0].id} src={Arrow} alt="arrow-down" className="indicator"></img>
           </button>
         </header>
-        <InfoSection title="First and Foremost" id="intro" type="secondary">
-          <p>I am a <span className="accent">Software Engineer</span>.</p>
+        <InfoSection title={Pages[0].name} id={Pages[0].id} type="secondary">
+          <p>I am a first and foremost a <span className="accent">Software Engineer</span>.</p>
+          <br />
+          <span>But I'm also a musician, consultant, certified French AND British AND American person, passionate buffalo sauce enthusist, and much more.</span>
+          <br/>
           <br/>
           <p>I have a passion for all things in the <span className="accent-secondary">web development</span> world. 
           I've built frontends from the ground up for a seed stage start up, and worked down in the weeds in <span className="accent-secondary">AWS</span>.</p>
           <br/>
           <p>Let's dive into specifics.</p>
           <button className="link-button">
-            <img section="lang" src={Arrow} alt="arrow-down" className="indicator" onClick={this.arrowClickHandler}></img>
+            <img section={Pages[1].id} src={Arrow} alt="arrow-down" className="indicator" onClick={this.arrowClickHandler}></img>
           </button>
         </InfoSection>
-        <InfoSection title="Languages" id="lang">
-          {createRatings(Languages, true, "#lang", "fade-right")}
+        <InfoSection title={Pages[1].name} id={Pages[1].id}>
+          <h2>Languages</h2>
+          {createRatings(Languages, true, Pages[1].id, "fade-right")}
         </InfoSection>
       </div>
     );
