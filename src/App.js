3c1a753f56cf0d131403c12f633bb4cf6e27e4e8
Will Taylor <taylorw@udel.edu>
2019-01-13 14:20:03 -0500

    More stylistic changes

--- src/App.js
+++ src/App.js
@@ -22,53 +22,39 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar></NavBar>
-        <div className="bgImage"></div>
-        <div className="content">
+        <div className="App-content">
           <HeroHeader title="Hi, I'm Will.">
             (scroll down to learn more about me)
-            <button onClick={this.arrowClickHandler} className="link-button">
+              <button onClick={this.arrowClickHandler} className="link-button">
               <img section={Pages[0].id} src={Arrow} alt="arrow-down" className="indicator"></img>
             </button>
           </HeroHeader>
-            {/* <InfoSection className="App-header">
-            <h1 className="main-header">Hi, I'm <span className="accent">Will</span>.</h1>
-            <h2 className="secondary-header">Let me tell you about myself.</h2>
-            <button onClick={this.arrowClickHandler} className="link-button">
-              <img section={Pages[0].id} src={Arrow} alt="arrow-down" className="indicator"></img>
+          <InfoSection title={Pages[0].name} id={Pages[0].id} type="secondary">
+            <p>I am a first and foremost a <span className="accent">Software Engineer</span>.</p>
+            <br />
+            <span>But I'm also a musician, consultant, certified French AND British AND American person, passionate buffalo sauce enthusist, and much more.</span>
+            <br />
+            <br />
+            <p>I have a passion for all things in the <span className="accent-secondary">web development</span> world.
+              I've built frontends from the ground up for a seed stage start up, and worked down in the weeds in <span className="accent-secondary">AWS</span>.</p>
+            <br />
+            <p>Let's dive into specifics.</p>
+            <button className="link-button">
+              <img section={Pages[1].id} src={Arrow} alt="arrow-down" className="indicator" onClick={this.arrowClickHandler}></img>
             </button>
-          </InfoSection> */}
-            <InfoSection title={Pages[0].name} id={Pages[0].id} type="secondary">
-              <p>I am a first and foremost a <span className="accent">Software Engineer</span>.</p>
-              <br />
-              <span>But I'm also a musician, consultant, certified French AND British AND American person, passionate buffalo sauce enthusist, and much more.</span>
-              <br />
-              <br />
-              <p>I have a passion for all things in the <span className="accent-secondary">web development</span> world.
-            I've built frontends from the ground up for a seed stage start up, and worked down in the weeds in <span className="accent-secondary">AWS</span>.</p>
-              <br />
-              <p>Let's dive into specifics.</p>
-              <button className="link-button">
-                <img section={Pages[1].id} src={Arrow} alt="arrow-down" className="indicator" onClick={this.arrowClickHandler}></img>
-              </button>
-            </InfoSection>
-            <InfoSection title={Pages[1].name} id={Pages[1].id} type="primary">
-              <div className="flex-row">
-                <Ratings title="Languages" data={ratingData.languages} parentElement={Pages[1].id} animation="fade-right" />
-                <Ratings title="Libraries" data={ratingData.libraries} parentElement={Pages[1].id} animation="fade-left" />
-              </div>
-            </InfoSection>
-            <InfoSection title={Pages[1].name} id={Pages[1].id} type="primary">
-              <div className="flex-row">
-                <Ratings title="Languages" data={ratingData.languages} parentElement={Pages[1].id} animation="fade-right" />
-                <Ratings title="Libraries" data={ratingData.libraries} parentElement={Pages[1].id} animation="fade-left" />
-              </div>
-            </InfoSection>
+          </InfoSection>
+          <InfoSection title={Pages[1].name} id={Pages[1].id} type="primary">
+            <div className="flex-row">
+              <Ratings title="Languages" data={ratingData.languages} parentElement={Pages[1].id} animation="fade-right" />
+              <Ratings title="Libraries" data={ratingData.libraries} parentElement={Pages[1].id} animation="fade-left" />
+            </div>
+          </InfoSection>
         </div>
       </div>
-        );
-      }
-    }
-    
-    
-    
-    export default App;
+    );
+  }
+}
+
+
+
+export default App;
