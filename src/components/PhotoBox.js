d052a6a805b1c6ad497feeeebf27d89c8b0455ee
Will Taylor <taylorw@udel.edu>
2019-01-29 23:56:02 -0500

    Added photo box component

--- src/components/PhotoBox.js
+++ src/components/PhotoBox.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Img = styled.div`
-    background-color: gray;
-    background-image: ${props => props.image};
-    width: ${props => props.width || "auto"};
-    height: ${props => props.width || "auto"};
+const Img = styled.img`
     border-radius: 10px;
+    min-height: 200px;
+    width: ${props => props.width || '100%'};
+    height: ${props => props.width || '25vw'};
+    object-fit: cover;
+
+    &:hover {
+        transition: .5s;
+        filter: brightness(130%);
+    }
 `
 
-const PhotoBox = ({image, width}) => (
-    <Img width={width} image={image} />
+const Container = styled.div`
+    min-width: 200px;
+    min-height: 250px;
+    width: ${props => props.width || '25vw'};
+    /* height: ${props => props.height || '40vw'}; */
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: #555;
+    text-align: center;
+`
+
+const Text = styled.div`
+    display: block;
+    padding: 12px;
+    font-size: calc(6px + 2vmin);
+`
+
+const PhotoBox = ({children, image, width, height}) => (
+    <Container width={width} height={height}>
+        <Img src={image} />
+        <Text>{children}</Text>
+    </Container>
 )
 
 export default PhotoBox;
--- src/containers/App.js
+++ src/containers/App.js
@@ -9,6 +9,11 @@ import { purple, cyan, } from '@material-ui/core/colors';
 import Section from '../components/Section';
 import TechTable from '../components/TechTable';
 import tech from '../tech.json';
+import PhotoBox from '../components/PhotoBox';
+import zeus from '../zeus.jpeg'
+import clog from '../clog.jpg';
+import selfie from '../selfie.jpg';
+import Grid from '@material-ui/core/Grid'
 
 const AppContainer = styled.div`
   color: white;
@@ -58,7 +63,7 @@ class App extends Component {
             minHeight="70vh" isFixed={true} />
           <Container backgroundColor={theme.palette.primary.dark} padding="24px 24px 200px 24px">
             <Section title="About Me">
-              Like I said up there, I'm Will. I'm a front-end developer who works mostly in React. I'm currently located in the Washington, D.C.
+              Like I said up there, I'm Will. I'm a software developer who works mostly in JavaScript. I'm currently located in the Washington, D.C.
               area working for Macedon Technologies. I've been programming since I was 12, and have been working on web tech for the past 4 years.
             </Section>
           </Container>
@@ -66,32 +71,40 @@ class App extends Component {
             <TechTable languages={tech.languages} tools={tech.tools} />
             <Section title="Experience">
               For the past nearly 2 years, I have been working as a Senior Technical Consultant at Macedon Technologies. There, I have been designing, building,
-              and devliering software to companies and government agencies. I have had the ability to lead small teams of developers, and rack up plenty
+              and delivering software to companies and government agencies. I have had the ability to lead small teams of developers, and rack up plenty
               of face time with the clients. It's been an incredible place to learn and grow.
               <br />
               <br />
               Previously, I worked at a seed stage start up called Cyber 20/20. As one of the first employees, I was tasked with building
               the front end from the ground up. The application integrated with a state of the art machine learning system, and was used
-              to show users what types of malware were incoming on their network. The front end was built in Angular, and run as an Electron app.
+              to show users what types of malware was incoming on their network. The front end was built in Angular, and run as an Electron app.
             </Section>
           </Container>
           <Container backgroundColor={theme.palette.primary.dark} padding="24px 24px 200px 24px">
             <Section title="Beyond Work">
-              Having been born in France to a french monther and english father has meant that my life largely revolves around travel. I've been to countless countries on 5 continents where I've had the ability to interact with so many incredible people.
+              Living in America while having been born in France to a french mother and english father means my life has always revolved around travel. 
+              I've been to countless countries on 5 continents where I've had the ability to interact with so many incredible people.
               <br />
               <br />
-              Aside from programming, my other passions in life are music, and my dog Zeus. I have been playing guitar and making music for 12 years,
+              Aside from programming, my other passions in life are music and my dog Zeus. I have been playing guitar and making music for 12 years,
               playing in bands on and off.
               <br />
               Zeus is my mutt puppy who I rescued in July 2018. He's the best - super cuddly -
               and if you work with me you might just get to meet him.
             </Section>
           </Container>
-          <Container>
-            {/* <Grid container direction="row" alignItem="center" >
-              <PhotoBoxGrid photos={[espana, lincoln]} />
-              <SectionContainer sections={data.experience} />
-            </Grid> */}
+          <Container margin="-200px 0px 0px 0px">
+            <Grid container direction="row" justify="center" spacing={24}>
+              <Grid item>
+                <PhotoBox image={zeus}>Zeus looking like he's finally accepted that this is his life now</PhotoBox>
+              </Grid>
+              <Grid item>
+                <PhotoBox image={selfie}>You have no idea how cold I was in this</PhotoBox>
+              </Grid>
+              <Grid item>
+                <PhotoBox image={clog}>There's probably a sweet joke using 'Clog' that I could've made here</PhotoBox>
+              </Grid>
+            </Grid>
           </Container>
         </AppContainer>
       </MuiThemeProvider>
