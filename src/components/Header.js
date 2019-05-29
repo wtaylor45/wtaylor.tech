dd58b914146a2de17e74e36b463d1f7f5430e056
Will Taylor <taylorw@udel.edu>
2019-05-29 19:23:31 -0400

    Add pause animation button

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ const Header = () => (
       >
         Blog
       </Link>
-      <div class="bar" />
+      <div className="bar" />
     </nav>
   </header>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/layout';
 import './index.css';
 
-const IndexPage = () => (
-  <Layout>
-    <section className="container">
-      <h1 className="title">
-        <span className="word">Will</span>
-        <span className="word bottom">Taylor</span>
-      </h1>
-      <h2 className="subtitle">
-        Front End Developer{' '}
-        <span role="img" aria-label="rocket">
-          🚀
-        </span>
-        Tech Enthusiast{' '}
-        <span role="img" aria-label="computer">
-          🖥
-        </span>
-      </h2>
-    </section>
-  </Layout>
-);
+const IndexPage = () => {
+  let [animationState, setAnimationState] = useState(true);
+  let [clicks, setClickCount] = useState(0);
+
+  const clickHandler = () => {
+    console.log(clicks);
+    setAnimationState(!animationState);
+    setClickCount(++clicks);
+  };
+
+  return (
+    <Layout>
+      <section className="container">
+        <h1
+          className={
+            'title ' +
+            (!animationState ? 'paused ' : '') +
+            (clicks > 20 ? 'awesome ' : '')
+          }
+        >
+          <span className="word">Will</span>
+          <span className="word bottom">Taylor</span>
+        </h1>
+        <h2 className="subtitle">
+          Front End Developer{' '}
+          <span role="img" aria-label="rocket">
+            🚀
+          </span>
+          Tech Enthusiast{' '}
+          <span role="img" aria-label="computer">
+            🖥
+          </span>
+        </h2>
+        <button
+          className={'pause-button ' + (!animationState ? 'paused ' : '')}
+          onClick={clickHandler}
+        />
+      </section>
+    </Layout>
+  );
+};
 
 export default IndexPage;
