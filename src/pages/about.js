29527887bd6f2ffbab420bdc6e4d24765b27d2ee
Will Taylor <taylorw@udel.edu>
2019-06-02 18:27:01 -0400

    Add content to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,12 +10,28 @@ const About = () => (
       <Title textAlign="center">
         <span styleName="title">About Me</span>
       </Title>
-      <section>
+      <section styleName="text">
         <p>
-          Hi, I'm Will! I'm a font end developer who works mostly in JavaScript.
+          Hi, I'm Will! I'm a font end developer who's passionate about creating
+          awesome experiences for all users. Over the past year, I've become
+          addicted to learning more about becoming a better developer and
+          communicator. Wether it's diving into the world of accessibility,
+          color theory, or the latest framework, I'm always looking for new
+          things to learn.
+        </p>
+        <p>
           I'm currently located in the Washington, D.C. area working for
-          Inspire. I've been programming since I was 12, and have been working
-          on web tech for the past 3 years.
+          Inspire. There, I'm helping the team with things ranging from SEO, to
+          building greenfield projects using the latest and greatest technology.
+          Through this experience I've been learning a lot, and I've been trying
+          my best to{' '}
+          <a
+            styleName="inline-link"
+            href="https://www.swyx.io/writing/learn-in-public/"
+          >
+            learn in public
+          </a>{' '}
+          (so check out my twitter!).
         </p>
       </section>
     </div>
