1be5b4a94a64c9193123b8add8fe28237982987d
Will Taylor <taylorw@udel.edu>
2019-06-03 08:28:30 -0400

    Fix typos on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,19 +10,19 @@ const About = () => (
       <h1 styleName="title">About Me</h1>
       <section styleName="text">
         <p>
-          Hi, I'm Will! I'm a font end developer who's passionate about creating
-          awesome experiences for all users. Over the past year, I've become
-          addicted to learning more about becoming a better developer and
-          communicator. Wether it's diving into the world of accessibility,
-          color theory, or the latest framework, I'm always looking for new
-          things to learn.
+          Hi, I'm Will! I'm a <b>Front End Developer</b> who's passionate about
+          creating awesome experiences for all users. Over the past year, I've
+          become addicted to learning more about becoming a better developer and
+          communicator. Whether it's diving into the world of accessibility,
+          color theory, or whatever the latest framework is, I'm always looking
+          for the next challenge.
         </p>
         <p>
           I'm currently located in the Washington, D.C. area working for
-          Inspire. There, I'm helping the team with things ranging from SEO, to
-          building greenfield projects using the latest and greatest technology.
-          Through this experience I've been learning a lot, and I've been trying
-          my best to{' '}
+          Inspire. There, I'm helping the team with anything ranging from SEO,
+          to building greenfield projects using the latest and greatest
+          technology. Through this experience I've been learning a lot, and I've
+          been trying my best to{' '}
           <a
             styleName="inline-link"
             href="https://www.swyx.io/writing/learn-in-public/"
