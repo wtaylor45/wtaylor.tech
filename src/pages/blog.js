1c3b5e9b2b0a3eba2656c69b3be5b8ab64088589
Will Taylor <taylorw@udel.edu>
2019-06-05 19:30:54 -0400

    Add graphql to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/SEO';
+import { graphql, Link } from 'gatsby';
 
-export const blog = () => {
-  return <div />;
-};
+const Blog = ({ data }) => (
+  <Layout>
+    <SEO title="Blog" />
+    <h1>Latest Posts</h1>
+    {data.allMarkdownRemark.edges.map(post => (
+      <div key="post.node.id">
+        <h3>{post.node.frontmatter.title}</h3>
+        <small>Posted on {post.node.frontmatter.date}</small>
+        <Link to={post.node.frontmatter.path}>Read More</Link>
+      </div>
+    ))}
+  </Layout>
+);
+
+export const pageQuery = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            author
+            path
+            date
+            title
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
+
+export default Blog;
