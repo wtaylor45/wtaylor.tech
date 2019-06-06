0bf5347b5635036f6ce309189faa55d04335023e
Will Taylor <taylorw@udel.edu>
2019-06-05 22:29:39 -0400

    Style blog post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import Layout from '../components/layout';
+import SEO from '../components/SEO';
 import { graphql, Link } from 'gatsby';
+import './blog-post.module.css';
 
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark;
 
   return (
     <Layout>
-      <Link to="/blog">Go Back</Link>
+      <SEO title={post.frontmatter.title} />
       <article>
-        <h1>{post.frontmatter.title}</h1>
-        <section dangerouslySetInnerHTML={{ __html: post.html }} />
+        <Link to="/blog" styleName="return">
+          Back
+        </Link>
+        <h1 styleName="title">{post.frontmatter.title}</h1>
+        <small styleName="subtitle">{post.frontmatter.date}</small>
+        <hr />
+        <section
+          dangerouslySetInnerHTML={{ __html: post.html }}
+          styleName="content"
+        />
       </article>
     </Layout>
   );
