b838acb96c7c8547b4b5009abbec83dac2afb086
Will Taylor <taylorw@udel.edu>
2019-06-05 22:49:12 -0400

    Style blog list

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,18 +2,26 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/SEO';
 import { graphql, Link } from 'gatsby';
+import './blog.module.css';
 
 const Blog = ({ data }) => (
   <Layout>
     <SEO title="Blog" />
-    <h1>Latest Posts</h1>
-    {data.allMarkdownRemark.edges.map(post => (
-      <div key="post.node.id">
-        <h3>{post.node.frontmatter.title}</h3>
-        <small>Posted on {post.node.frontmatter.date}</small>
-        <Link to={post.node.frontmatter.path}>Read More</Link>
-      </div>
-    ))}
+    <div styleName="container">
+      <h1 styleName="title">Blog Posts</h1>
+      {data.allMarkdownRemark.edges.map(post => (
+        <article key="post.node.id">
+          <div styleName="frontmatter">
+            <h3 styleName="post-title">{post.node.frontmatter.title}</h3>
+            <small>{post.node.frontmatter.date}</small>
+          </div>
+          {post.node.excerpt}
+          <Link styleName="read-more" to={post.node.frontmatter.path}>
+            Read More
+          </Link>
+        </article>
+      ))}
+    </div>
   </Layout>
 );
 
---
+++ src/posts/test2.md
@@ -0,0 +1,12 @@
+---
+path: "/test-two"
+date: "2019-06-05"
+title: "A Test Post!"
+author: "Will Taylor"
+---
+
+Here is my test post. This is a longer post and you have no idea how much it is!!
+
+```javascript
+const test = () => 3+2;
+```
